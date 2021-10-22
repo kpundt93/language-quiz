3124bda992267d4f9c367fc1032b5171973a12f9
Katie Pundt <kzpundt@gmail.com>
2021-10-22 10:47:57 -0700

    add branching for question 1

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,16 @@ $(document).ready(function() {
     const q4 = $("input:radio[name=q4]:checked").val();
     const q5 = $("input:radio[name=q5]:checked").val();
     const q6 = $("input:radio[name=q6]:checked").val();
+    let total = 0;
+
+    if (q1 === "green") {
+      total += 1
+    } else if (q1 === "yellow") {
+      total += 2
+    } else if (q1 === "blue") {
+      total += 3
+    } else if (q1 === "red") {
+      total += 4
+    }
   })
 });
